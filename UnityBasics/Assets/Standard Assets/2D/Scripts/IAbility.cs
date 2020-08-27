5d7e73b2daa8a8674d6ceb47f82d3a68932f97f2
Halil Coşgun <cosgun.halil@gmail.com>
2020-08-27 10:46:08 +0300

    refactor: Divide abilities
    #Jumping ability
    # RunningAbility
    # Crouch ability

---
+++ UnityBasics/Assets/Standard Assets/2D/Scripts/IAbility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public abstract class Ability
+{
+    protected PlatformerCharacter2D character;
+
+    public Ability(PlatformerCharacter2D platformerCharacter2D)
+    {
+        this.character = platformerCharacter2D;
+    }
+
+    public abstract void AbilityFixedUpdate();
+}
---
+++ UnityBasics/Assets/Standard Assets/2D/Scripts/MovementAbility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class MovementAbility : Ability
+{
+    public MovementAbility(PlatformerCharacter2D platformerCharacter2D) : base(platformerCharacter2D)
+    {
+        this.character = platformerCharacter2D;
+    }
+
+    public override void AbilityFixedUpdate()
+    {
+        //only control the player if grounded or airControl is turned on
+        if (character.IsGrounded() || character.IsAirControlerActive())
+        {
+            var move = character.GetMove();
+
+            character.SetSpeedValueToAnimator(Mathf.Abs(move));
+
+            Vector2 characterVelocity = character.GetVelocity();
+
+            character.SetVelocity(new Vector2(move * character.GetMaxSpeed(), characterVelocity.y));
+
+            HandleFacing();
+        }
+    }
+
+    private void HandleFacing()
+    {
+        // If the input is moving the player right and the player is facing left...
+        if (character.GetMove() > 0 && !character.GetIsFacingRight())
+        {
+            // ... flip the player.
+            character.Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (character.GetMove() < 0 && character.GetIsFacingRight())
+        {
+            // ... flip the player.
+            character.Flip();
+        }
+    }
+}
